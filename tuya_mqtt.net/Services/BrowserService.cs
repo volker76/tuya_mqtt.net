b9338c541ebc699ce4dc42e88c55f4851a2b1eaa
Volker Hänsel <volker@haensel-online.de>
2023-10-04 19:43:57 +0200

    move BrowserService and take out all resize functions, add download functions

---
+++ tuya_mqtt.net/Services/BrowserService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics;
+using tuya_mqtt.net.Data;
+
+namespace tuya_mqtt.net.Services
+{
+    public class BrowserService : IAsyncDisposable
+    {
+
+        private readonly ILogger _logger;
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public BrowserService(ILogger<BrowserService> logger, IJSRuntime jsRuntime)
+        {
+            _logger = logger;
+
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "/lib/BrowserService.js").AsTask());
+        }
+
+        public async Task<TimeSpan> GetTimeZoneOffset()
+        {
+            var module = await moduleTask.Value;
+            int i = await module.InvokeAsync<int>("timeZoneOffset");
+            return TimeSpan.FromMinutes(-i);
+
+        }
+
+        public async Task<string> GetBrowserLanguage()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<string>("getBrowserLanguage");
+        }
+        
+        public async Task ScrollClassIntoView(string classSearchString, int index)
+        {
+            var module = await moduleTask.Value;
+
+            await module.InvokeVoidAsync("scrollToElement", new object[] { classSearchString, index });
+        }
+
+        public async Task DownloadStreamAsync(string fileName, Stream fileStream)
+        {
+            var module = await moduleTask.Value;
+
+            using var streamRef = new DotNetStreamReference(stream: fileStream);
+
+            await module.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
+        }
+
+
+        private bool disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //close any events ....
+                }
+                disposed = true;
+            }
+        }
+
+
+
+        public async ValueTask DisposeAsync()
+        {
+            try
+            {
+                if (moduleTask.IsValueCreated)
+                {
+                    var module = await moduleTask.Value;
+                    await module.DisposeAsync();
+                }
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            catch (Exception)
+            {
+                // https://stackoverflow.com/questions/72488563/blazor-server-side-application-throwing-system-invalidoperationexception-javas
+            }
+        }
+
+        
+    }
+}
--- tuya_mqtt.net/wwwroot/lib/BrowserService.js
+++ tuya_mqtt.net/wwwroot/lib/BrowserService.js
@@ -1,33 +1,5 @@
 ﻿// functions for BrowserService.cs
 
-// listen for page resize
-export function resizeListener(dotnethelper) {
-    $(window).on("resize", function () {
-        let browserHeight = $(window).innerHeight();
-        let browserWidth = $(window).innerWidth();
-        dotnethelper.invokeMethodAsync('SetBrowserDimensions', browserWidth, browserHeight).then(() => {
-            // success, do nothing
-        }).catch(error => {
-            console.log("Error during browser resize: " + error);
-        });
-    });
-}
-
-export function viewportSize() {
-    let Height = $(window).innerHeight();
-    let Width = $(window).innerWidth();
-    return [Width, Height];
-}
-
-//get dimensions of a HTML object
-export function MyGetBoundingClientRect(element)
-{
-    return element.getBoundingClientRect().catch(error => {
-        console.log("Error retrieving getBoundingClientRect: " + error);
-    });
-}
-
-
 export function scrollToElement(elem,index) {
     $(elem).get(index).scrollIntoView()
 }
@@ -39,4 +11,16 @@ export function timeZoneOffset()  {
 export function getBrowserLanguage() {
     return (navigator.languages && navigator.languages.length) ? navigator.languages[0] :
         navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
+}
+
+export function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
 }
